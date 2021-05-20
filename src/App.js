e9c20e6a3182557ba5cea8161c48c1f51df9ff88
Soumya <soumyatarafder624@gmail.com>
2021-05-20 23:11:58 +0530

    abc

--- src/App.js
+++ src/App.js
@@ -10,19 +10,35 @@ export default function App(props) {
         <legend>Notification Type:</legend>
         <FormGroup check>
           <Label check>
-              <Input type="radio" name="radio1" />{' '}
+              <Input type="radio" name="radio1" onClick={function(){
+                var rad1= new Array();
+                rad1[0]=prompt('Enter the Title:');
+                rad1[1]=prompt('Enter the Message:');
+                console.log(rad1);
+              }}/>{' '}
               Title and Message
           </Label>
         </FormGroup>
         <FormGroup check>
           <Label check>
-            <Input type="radio" name="radio1" />{' '}
+            <Input type="radio" name="radio1" onClick={function(){
+              var rad2= new Array();
+              rad2[0]=prompt('Enter the Title:');
+              rad2[1]=prompt('Enter the Start Time (HH:MM:SS): ');
+              rad2[2]=prompt('Enter the End Time (HH:MM:SS): ');
+              console.log(rad2);
+            }}/>{' '}
             Title, Start time and End time
           </Label>
         </FormGroup>
         <FormGroup check disabled>
           <Label check>
-            <Input type="radio" name="radio1" />{' '}
+            <Input type="radio" name="radio1" onClick={function(){
+              var rad3= new Array();
+              rad3[0]=prompt('Enter the Title:');
+              rad3[1]=prompt('Enter the Start Time (HH:MM:SS): ');
+              console.log(rad3);
+            }}/>{' '}
             Title and Start time
           </Label>
         </FormGroup>

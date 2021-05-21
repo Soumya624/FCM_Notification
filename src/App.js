75bde4186d3984138ae6943ad8523f8f2f51427b
Soumya Tarafder <63846042+Soumya624@users.noreply.github.com>
2021-05-21 16:07:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import firebase from './firebase'
 
 export default function App(props) {
+  const [ token , setToken ] = useState('')
+
   const [disabled, setDisable] = useState(true)
   const [disabled1, setDisable1] = useState(true)
   const [disabled2, setDisable2] = useState(true)
@@ -15,28 +17,59 @@ export default function App(props) {
   const [ time1 , setTime1] = useState('')
   const [ time2 , setTime2] = useState('')
 
+//   {
+//     "id": 3,
+//     "url": "https://fcm-testing.herokuapp.com/api/fcm_devices/3",
+//     "name": "Subhojit Chrome",
+//     "active": true,
+//     "date_created": "2021-05-21T12:21:52.081418+05:30",
+//     "device_id": "3",
+//     "registration_id": "d1whUyK5bYxnYzNDSHitK3:APA91bFapWPuA-zhI_OiQcyAQxSCmBirQWfUhdM-Cbigq-za9k1SKz9nnxORy9gSxG9KGNzXWMFa3Pqkskf_bPm45wZn_2kOeGV6WDRX2GzZ_JqfYMpE-mSkPPX4HIjbbKfBVdglneu6",
+//     "type": "web",
+//     "user": 3
+// }
+
+  function setUserDevice(itoken){
+    axios.post('https://fcm-testing.herokuapp.com/api/fcm_devices/',
+    {
+      "name":"Chrome",
+      "registration_id": itoken,
+      "type":"web"
+    }
+    ).then(res=>console.log(res))
+    .catch(err=>console.log('Error',err))
+    alert('User is registered')
+  }
 
   useEffect(()=>{
-    // axios.post('https://fcm-testing.herokuapp.com/api/send_message/',
-    // {
-    //   title: "sample_title (This is Required)",
-    //   message: "sample_msg (This is Required)",
-    // }
-    // ).then(function (response) {
-    //   console.log(response);
-    // })
     const messaging= firebase.messaging()
     messaging.requestPermission().then((token)=>{
       return messaging.getToken()
     }).then(token=>{
       console.log('Token: ', token);
+      axios.get(`https://fcm-testing.herokuapp.com/api/check_registration?reg_id=${token}`)
+      .then(res =>{
+        res.data.is_registered === false ? setUserDevice(token) : alert("You are already registered")
+      })
     }).catch(()=>{
       console.log('Error!')
     })
     messaging.onMessage(function(payload){
             console.log('onMessage: ',payload);
     })
+
+
+    // window.addEventListener('load',()=>{
+    //   axios.get(`https://fcm-testing.herokuapp.com/api/check_registration?reg_id=${token}`)
+    //   .then(res =>{
+    //     res.data.is_registered === false ? setUserDevice(token) : alert("You are already registered")
+    //   })
+    // })
   },[])
+
+
+  
+
 
   function submitMessage(e){
     e.preventDefault();
